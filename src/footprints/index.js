4954a97e7234b3157151fdbd939864a0b5d021db
eugen <dillinverser@icloud.com>
2022-12-21 17:55:38 -0600

    added xiao and mh footprints

--- src/footprints/index.js
+++ src/footprints/index.js
@@ -17,4 +17,6 @@ module.exports = {
     slider: require('./slider'),
     trrs: require('./trrs'),
     via: require('./via'),
+    mountinghole: require('./mountinghole'),
+    xiao: require('./xiao'),
 }
